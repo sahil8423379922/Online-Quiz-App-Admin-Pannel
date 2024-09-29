ff4aab0a4f7843dcbb15ff2d54ab532a6be65c33
sahil8423379922 <sahil1905050310026@gmail.com>
2024-09-29 22:06:42 +0530

    code

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
 export default function Dashboard() {
+
+const operations =[
+    {name:"Live Quiz"},
+    {name:"Mock Quiz"},
+    {name:"Total User"},
+    {name:"Total Revenue"},
+    {name:"Paid User"},
+    {name:"Free User"}
+]
+
   return (
-    <div>Dashboard</div>
+    <>
+  <div className='container'>
+  <div className='row'>
+    {operations.map((val) => (
+      <div className='card' key={val.id} style={{paddingTop:"20px", paddingBottom:'20px',fontSize:"20px", marginBottom:"10px",marginRight:"20%",marginLeft:"20%"}}> {/* Add horizontal margin */}
+        <span>{val.name}</span>
+      </div>
+    ))}
+  </div>
+</div>
+
+
+    </>
   )
 }
